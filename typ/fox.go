4ceb4119d8e43f134ad8b7cadb4bdde2f7a1783c
Evan.Lu <evan.lu@lianwei.com.cn>
2022-10-14 11:47:31 +0800

    new version 0.0.1.8

--- typ/fox.go
+++ typ/fox.go
@@ -2,8 +2,6 @@ package typ
 
 import (
 	"github.com/iEvan-lhr/worker/res"
-	"log"
-	"runtime"
 	"sync"
 	"time"
 )
@@ -29,7 +27,6 @@ func (f *FoxExecutor) DoMaps() chan struct{} {
 		go func(index int) {
 			<-f.Master[index]
 			f.DoMap.Delete(index)
-			f.CheckUseChan()
 		}(f.i)
 		if f.i != res.MasterLen-1 {
 			f.i++
@@ -37,7 +34,6 @@ func (f *FoxExecutor) DoMaps() chan struct{} {
 			f.i = 0
 		}
 	}
-	log.Println("调度", f.i)
 	return f.Master[f.i]
 }
 
@@ -50,22 +46,19 @@ NEXT:
 			go func(index int) {
 				<-f.Master[index]
 				f.DoMap.Delete(index)
-				f.CheckUseChan()
 			}(i)
-			log.Println("调度", i)
 			return f.Master[i]
 		}
 	}
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(3 * time.Millisecond)
 	goto NEXT
 }
 
-func (f *FoxExecutor) CheckUseChan() {
-	sum := 0
-	for i := 0; i < res.MasterLen; i++ {
-		if _, ok := f.DoMap.Load(i); !ok {
-			sum++
-		}
-	}
-	log.Println("可用协程数量为", sum, " 当前使用中的协程数量为", runtime.NumGoroutine())
-}
+//func (f *FoxExecutor) CheckUseChan() {
+//	sum := 0
+//	for i := 0; i < res.MasterLen; i++ {
+//		if _, ok := f.DoMap.Load(i); !ok {
+//			sum++
+//		}
+//	}
+//}
