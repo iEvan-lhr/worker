0401e415d6ac40affea2d155d234e455f2fcf54e
Evan.Lu <evan.lu@lianwei.com.cn>
2022-10-24 17:01:11 +0800

    new version 0.0.2.1

--- res/config.go
+++ res/config.go
@@ -9,5 +9,5 @@ func ReSetMasterLen(i int) {
 }
 
 func init() {
-	MasterLen = runtime.NumCPU() * 6
+	MasterLen = runtime.NumCPU() * 2
 }
--- typ/fox_test.go
+++ typ/fox_test.go
@@ -13,16 +13,16 @@ func TestInit(t *testing.T) {
 	sum := 0
 	f.Init()
 	for i := 0; i < 16; i++ {
-		maps := f.DoMaps()
-		go func(m chan struct{}, index int) {
-			time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		go func(index int) {
+			m := f.DoMaps()
+			time.Sleep(time.Duration(rand.Intn(2)) * time.Millisecond)
 			m <- struct{}{}
-		}(maps, i)
+		}(i)
 		if i == 15 {
 			i = 0
 		}
 		sum++
-		if sum == 90000 {
+		if sum == 9000 {
 			break
 		}
 	}
