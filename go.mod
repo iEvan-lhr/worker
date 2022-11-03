45102235b481293a301fb41d4a7a49057f7b683b
Evan.Lu <evan.lu@lianwei.com.cn>
2022-11-03 10:11:30 +0800

    add 环境自定义修正

--- go.mod
+++ go.mod
@@ -5,12 +5,4 @@ go 1.18
 require (
 	github.com/iEvan-lhr/nihility-dust v0.1.3-0.20221031070659-7859a18e273b
 	github.com/iEvan-lhr/string v0.0.4
-	gorm.io/driver/mysql v1.3.4
-	gorm.io/gorm v1.23.6
-)
-
-require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/iEvan-lhr/nihility-dust/anything"
-	"github.com/iEvan-lhr/worker/db"
 	"github.com/iEvan-lhr/worker/engine"
 	"github.com/iEvan-lhr/worker/model"
 	"github.com/iEvan-lhr/worker/router"
@@ -12,5 +11,5 @@ func main() {
 	e := engine.Engine{
 		W: anything.Wind{},
 	}
-	e.Start("9080", []any{&db.Conn{}, &db.WorkerInfo{}, &model.User{}}, []any{&router.Router{}})
+	e.Start("9080", []any{&model.User{}}, []any{&router.Router{}})
 }
